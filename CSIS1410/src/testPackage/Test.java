84860ae3d8db010521b4f864e3943c62b4627e01
aleksandr-sidorenko <aleksasidorenko@gmail.com>
2022-09-01 22:26:20 -0600

    test

--- CSIS1410/src/testPackage/Test.java
+++ CSIS1410/src/testPackage/Test.java
@@ -1,10 +1,10 @@
 package testPackage;
 
-public class HelloWorld {
+public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.print("Hello World!");
 
 	}
 
