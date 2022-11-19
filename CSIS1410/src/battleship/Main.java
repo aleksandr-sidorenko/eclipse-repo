a569603bf5cb193b7d27ad463332f825f7b7dbe5
aleksandr-sidorenko <aleksasidorenko@gmail.com>
2022-11-18 18:49:23 -0700

    Added ASCII

--- CSIS1410/src/battleship/Main.java
+++ CSIS1410/src/battleship/Main.java
@@ -1,11 +1,29 @@
 package battleship;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.print("Hello Battleship!");
 
-	}
+	
+	
+	try {
+        BufferedReader reader = new BufferedReader(new FileReader("/home/aleks/git/eclipse-repo/CSIS1410/res/ASCII.txt"));
+        String text;
+        while((text = reader.readLine()) != null) {
+            System.out.println(text);
+        }
+        //System.out.println(reader.readLine());
+        reader.close();
 
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+}
 }
